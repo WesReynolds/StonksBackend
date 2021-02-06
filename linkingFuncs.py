196fac7dcaaf95b69f47b98c52cddfe70e5ae8e8
Wesley Reynolds <wesr@Wesleys-Mac-mini.local>
2021-02-06 00:43:40 -0800

    Added linkingFuncs.py

---
+++ linkingFuncs.py
@@ -0,0 +1,8 @@
+# Given a Ticker symbol and Timestring, this function will
+# return an Ordered Tuple of the form for the time frame:
+# (currentPrice, lowPrice, highPrice)
+def getDetailedStats(tckr, time):
+    currentPrice = 0
+    lowPrice = 0
+    highPrice = 0
+    return (currentPrice, lowPrice, highPrice)
